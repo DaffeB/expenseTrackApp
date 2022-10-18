fec04f1cf0bb07a4b05249264bdc8d8ab8de82f5
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-10-18 11:40:06 +0200

    /

--- expenseApp/components/ExpenseOutput/ExpenseItem.js
+++ expenseApp/components/ExpenseOutput/ExpenseItem.js
@@ -8,7 +8,7 @@ function ExpenseItem({ description, amount, date }) {
             <View style={styles.expenseItem}>
                 <View>
                     <Text style={[styles.textBase, styles.description]}>{description}</Text>
-                    <Text style={styles.textBase}>{date}</Text>
+                    <Text style={styles.textBase}>{date.toString()}</Text>
                 </View>
                 <View style={styles.amountContainer}>
                     <Text style={styles.amount}>{amount}</Text>
