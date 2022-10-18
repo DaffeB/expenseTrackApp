15c57929d45f03ae262a927d7c4d44d73db95091
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-10-18 10:51:42 +0200

    Adding some dummy data

--- expenseApp/components/ExpenseOutput/ExpenseOutput.js
+++ expenseApp/components/ExpenseOutput/ExpenseOutput.js
@@ -2,6 +2,39 @@ import { FlatList, View } from 'react-native'
 import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2022-10-03')
+    },
+    {
+        id: 'e2',
+        description: 'A pair of trousers',
+        amount: 99.99,
+        date: new Date('2022-10-05')
+    },
+    {
+        id: 'e3',
+        description: 'Some bananas',
+        amount: 5.99,
+        date: new Date('2022-09-10')
+    },
+    {
+        id: 'e4',
+        description: 'A book',
+        amount: 14.99,
+        date: new Date('2022-02-19')
+    },
+    {
+        id: 'e5',
+        description: 'Another book',
+        amount: 18.99,
+        date: new Date('2022-02-22')
+    },
+]
+
 function ExpenseOutput({ expenses, expensesPeriod }) {
     return (
         <View>
