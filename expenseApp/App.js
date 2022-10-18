7b7d5212d27ef7e8a776bbf1cf034c76e46ece5f
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-10-18 08:50:43 +0200

    /

--- expenseApp/App.js
+++ expenseApp/App.js
@@ -3,35 +3,30 @@ import React from 'react';
 
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
 
 const App = () => {
 
   return (
-    <SafeAreaView>
+    <View style={styles.container}>
+      <Text>Hello</Text>
+      <StatusBar style='auto' />
+    </View>
 
-
-    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 
 });
 
