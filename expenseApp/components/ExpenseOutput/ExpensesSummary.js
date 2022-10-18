711aa26f774bbe790e66cf993f58ce1c0143b485
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-10-18 10:38:58 +0200

    /

--- expenseApp/components/ExpenseOutput/ExpensesSummary.js
+++ expenseApp/components/ExpenseOutput/ExpensesSummary.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native';
 
 function ExpensesSummary({ periodName, expenses }) {
-    const expensesSum = expenses.reduce((sum, expenses) => { })
+    const expensesSum = expenses.reduce((sum, expenses) => {
+        return sum + expenses.amount
+    }, 0)
     return (
         <View>
             <Text>{periodName}</Text>
-            <Text>$</Text>
+            <Text>${expensesSum.toFixed(2)}</Text>
         </View>
     )
 
