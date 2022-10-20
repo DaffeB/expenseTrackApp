21120e459f51406a6c5039a4ca3c880f2e00c890
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-10-20 08:51:01 +0200

    Working with useReducer

--- expenseApp/store/expenses-context.js
+++ expenseApp/store/expenses-context.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer, useState } from "react";
 
 export const ExpenseContext = createContext({
     expenses: [],
@@ -6,3 +6,35 @@ export const ExpenseContext = createContext({
     deleteExpense: (id) => { },
     updateExpense: (id, { description, amount, date }) => { }
 })
+
+function expensesReducer(state, action) {
+    switch (action.type) {
+        case 'ADD':
+        case 'UPDATE':
+        case 'DELETE':
+        default:
+            return state
+
+    }
+}
+
+function ExpensesContextProvider({ children }) {
+    const [expensesState, dispatch] = useReducer(expensesReducer);
+
+    function addExpense(expenseData) {
+        dispatch({ type: 'ADD', payload: expenseData })
+    }
+
+    function deleteExpense(id) {
+        dispatch({ type: 'DELETE', payload: id })
+    }
+
+    function updateExpense(id, expenseData) {
+        dispatch({ type: 'UPDATE', payload: { id: id, data: expenseData } })
+    }
+
+    return <ExpenseContext.Provider>{children}</ExpenseContext.Provider>
+
+}
+
+export default ExpensesContextProvider;
