e68802079c899a6a3a37d832c00e13af20c409b6
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2022-10-22 08:30:52 +0200

    /

---
+++ expenseApp/components/ManageExpense/Input.js
@@ -0,0 +1,14 @@
+import { View, Text, TextInput } from 'react-native'
+
+
+
+function Input() {
+    return (
+        <View>
+            <Text>Label</Text>
+            <TextInput />
+        </View>
+    )
+}
+
+export default Input;
